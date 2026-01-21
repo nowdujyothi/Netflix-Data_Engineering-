84f9093f3f8cc18c649ee4013e98477f4713596d
nowdujyothi <nowdujyothi16@gmail.com>
2026-01-21 21:26:40 +0530

    Delete st_movies.sql

--- st_movies.sql
+++
@@ -1,9 +0,0 @@
-
-{{ config(materialized = 'table') }}
-with raw_movies as (
-    select * from netflix.raw.raw_movies
-)
-select distinct MOVIEID as movie_id,
-                TITLE,
-                GENRES
-    from raw_movies
